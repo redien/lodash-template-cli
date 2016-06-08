3a0795d11250dfb4dce15488c60f1e983030f0b2
Jesper Oskarsson <jesosk@gmail.com>
2016-06-08 13:42:08 +0200

    Added support for piping and multiple input files

--- bin/lodash-template-cli.js
+++ bin/lodash-template-cli.js
@@ -10,28 +10,60 @@
 // You should have received a copy of the CC0 Public Domain Dedication along with this software.
 // If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-var program = require('commander');
-var chalk = require('chalk');
 var fs = require('fs');
+var path = require('path');
+
+var shell = require('shelljs');
+var argv = require('minimist')(process.argv.slice(2));
+
 var templateTransformer = require('../template-transformer');
 
-program
-  .version('1.1.1')
-  .arguments('<input-data-file> <input-template-file> <output-file>')
-  .parse(process.argv);
+var exitWithHelp = function () {
+    console.log(
+    'usage: lodash-template-cli [options] <template>\n' +
+    '                           [<json-files>* <output-directory>]\n' +
+    '\n' +
+    'options:\n' +
+    '    -v, --version     Displays version.\n' +
+    '    -h, --help        Displays usage information.\n');
 
-if (program.args.length < 3) {
-    console.error(chalk.red('lodash-template-cli requires 3 arguments.\nSee lodash-template-cli --help for usage.'));
-    process.exit(-1);
+    process.exit(1);
+};
+
+if (argv.help || argv.h) {
+    exitWithHelp();
+
+} else if (argv.version || argv.v) {
+    var pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json')).toString());
+    console.log(pkg.version);
+
+} else if (argv._.length === 1) {
+    var templatePath = argv._[0];
+    var template = fs.readFileSync(templatePath).toString();
+
+    var inputStream = process.stdin;
+    var outputStream = process.stdout;
+
+    inputStream.pipe(templateTransformer.transformWith(template)).pipe(outputStream);
+
+} else if (argv._.length >= 3) {
+    var templatePath = argv._[0];
+    var template = fs.readFileSync(templatePath).toString();
+
+    var files = argv._.slice(1, -1);
+    var outputDirectory = argv._[argv._.length - 1];
+
+    files.forEach(function (file) {
+        var outputPath = path.join(outputDirectory, file);
+
+        shell.mkdir('-p', path.dirname(outputPath));
+
+        var inputStream = fs.createReadStream(file);
+        var outputStream = fs.createWriteStream(outputPath);
+
+        inputStream.pipe(templateTransformer.transformWith(template)).pipe(outputStream);
+    });
+
+} else {
+    exitWithHelp();
 }
-
-var dataPath = program.args[0];
-var templatePath = program.args[1];
-var outputPath = program.args[2];
-
-var inputStream = fs.createReadStream(dataPath);
-var outputStream = fs.createWriteStream(outputPath);
-
-var template = fs.readFileSync(templatePath).toString();
-
-inputStream.pipe(templateTransformer.transformWith(template)).pipe(outputStream);
